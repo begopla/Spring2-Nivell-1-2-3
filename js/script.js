6faa8f9fb32380b6f670858449157bc2b2d7e3fd
begopla <begonapr@gmail.com>
2022-08-26 18:57:41 +0400

    Nivell 1, 2 i 3

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+    let button =document.getElementsByClassName('btn btn-secondary login-button')
+    
+    // button.onmouseover = function(){
+    //     console.log(botton)
+    // }
+    
+    
+    // function mOver() {
+    //     console.log('hi')
+    //     button.classList.add("btn-outline-secondary")
+    // }
